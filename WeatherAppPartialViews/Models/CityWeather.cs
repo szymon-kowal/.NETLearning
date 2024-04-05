c27450de81f333db41d972aaa12c5291abf6969c
Szymon Kowal <00szymon.kowal@gmail.com>
2024-04-05 13:59:07 +0200

    Add partial views weather app

---
+++ WeatherAppPartialViews/Models/CityWeather.cs
@@ -0,0 +1,20 @@
+﻿namespace WeatherAppPartialViews.Models;
+
+public class CityWeather {
+
+	public string CityUniqeCode {
+		get; set;
+	} = string.Empty;
+
+	public string CityName {
+		get; set;
+	} = string.Empty;
+
+	public DateTime DateAndTime {
+		get; set;
+	}
+
+	public int TemperatureFahrenheit {
+		get; set;
+	}
+}
